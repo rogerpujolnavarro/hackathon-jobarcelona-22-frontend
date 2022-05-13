263d3f49ff5500ad9cf1198705e9377fab73c4ad
rogerpujolnavarro <rogerpujolnavarro@gmail.com>
2022-05-13 20:39:40 +0200

    Badge component with 3 sizes

--- src/stories/Badge.jsx
+++ src/stories/Badge.jsx
@@ -3,7 +3,7 @@ import './badge.css'
 
 export const Badge = ({ backgroundColor, color, label, size }) => {
 	return (
-		<div className={`badge${size ? ' badge-' + size : ''}`} style={{ backgroundColor, color }}>
+		<div className={`badge ${size ? 'badge-' + size : ''}`} style={{ backgroundColor, color }}>
 			{label}
 		</div>
 	)
@@ -14,12 +14,10 @@ Badge.propTypes = {
 	color: PropTypes.string,
 	label: PropTypes.string.isRequired,
 	size: PropTypes.oneOf(['sm', 'md', 'lg']),
-	onClick: PropTypes.func,
 }
 
 Badge.defaultProps = {
 	backgroundColor: '#198754',
 	color: '#f8f9fa',
 	size: 'md',
-	onClick: undefined,
 }
--- src/stories/Badge.stories.jsx
+++ src/stories/Badge.stories.jsx
@@ -3,14 +3,25 @@ import React from 'react'
 import { Badge } from './Badge'
 
 export default {
-	title: "JOBarcelona'22/Badge",
+	title: "JOBarcelona'22/GiftsList Components/Badge",
 	component: Badge,
 }
 
 const Template = (args) => <Badge {...args} />
 
-export const Default = Template.bind({})
-Default.args = {
+export const Medium = Template.bind({})
+Medium.args = {
 	label: 'Badge',
 	size: 'md',
+}
+
+export const Small = Template.bind({})
+Small.args = {
+	label: 'Badge',
+	size: 'sm',
+}
+export const Large = Template.bind({})
+Large.args = {
+	label: 'Badge',
+	size: 'lg',
 }
