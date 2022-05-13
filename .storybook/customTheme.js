b5f2f6771b852de95fbc156ab6ed48a3c4b4a0fc
rogerpujolnavarro <rogerpujolnavarro@gmail.com>
2022-05-13 20:28:43 +0200

    Styling storybook

---
+++ .storybook/customTheme.js
@@ -0,0 +1,35 @@
+import { create } from '@storybook/theming'
+
+export default create({
+	base: 'dark',
+	// colorPrimary: 'hotpink',
+	// colorSecondary: 'deepskyblue',
+
+	// // UI
+	// appBg: 'white',
+	// appContentBg: '#fff',
+	// appBorderColor: 'grey',
+	// appBorderRadius: 4,
+
+	// // Typography
+	// fontBase: '"Open Sans", sans-serif',
+	// fontCode: 'monospace',
+
+	// // Text colors
+	// textColor: 'black',
+	// textInverseColor: 'rgba(255,255,255,0.9)',
+
+	// // Toolbar default and active colors
+	// barTextColor: 'silver',
+	// barSelectedColor: 'black',
+	// barBg: '#ddd',
+
+	// // Form colors
+	// inputBg: 'white',
+	// inputBorder: 'silver',
+	// inputTextColor: 'black',
+	// inputBorderRadius: 40,
+
+	brandTitle: 'JOBX Front end',
+	brandUrl: 'https://rogerpujol.cat',
+})
